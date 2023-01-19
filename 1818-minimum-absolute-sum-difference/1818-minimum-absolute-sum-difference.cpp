61d500d95c92fc15d977537f6a078ca92d7167b0
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2023-01-19 13:35:27 +0530

    Time: 209 ms (87.65%), Space: 65.1 MB (53.24%) - LeetHub

---
+++ 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minAbsoluteSumDiff(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int M = 1e9 + 7;
+        int sum = 0;
+        vector<int> diff(n);
+        for(int i = 0; i < n; i++) {
+            diff[i] = abs(nums1[i] - nums2[i]);
+            //Avoid Integer Overflow Condition
+            sum = (sum + diff[i]) % M;
+        }
+        sort(nums1.begin(), nums1.end());
+        vector<int> arr(n);
+        for(int i = 0; i < n; i++) {
+            int j = lower_bound(nums1.begin(), nums1.end(), nums2[i]) - nums1.begin();
+            if(j != 0 && j != n) 
+                arr[i] = min(abs(nums2[i] - nums1[j]), abs(nums2[i] - nums1[j - 1]));
+            else if(j == 0) 
+                arr[i] = abs(nums2[i] - nums1[j]);
+            else if(j == n)
+                arr[i] = abs(nums2[i] - nums1[j - 1]);
+        }
+        int bestSave = 0;
+        for(int i = 0; i < n; i++) {
+            bestSave = max(bestSave, diff[i] - arr[i]);
+        }
+        return (M + sum - bestSave) % M;
+    }
+};
