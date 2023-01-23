d7b0cf89312aadb27b88f261e82caa42b624bafb
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2023-01-23 09:27:42 +0530

    Time: 393 ms (25.50%), Space: 60.7 MB (95.56%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,11 +1,23 @@
+// Make two arrays :-
+// a. arr1 for keeping count of who trusts ith member
+// b. arr2 for keeping count of all the people ith member trusts
+
+// The judge must trust noone i.e. arr1[i] == 0
+
+// All members must trust judge i.e. arr2[i] == n-1
+
+
 class Solution {
 public:
-      int findJudge(int N, vector<vector<int>>& trust) {
-        vector<int> count(N + 1, 0);
-        for (auto& t : trust)
-            count[t[0]]--, count[t[1]]++;
-        for (int i = 1; i <= N; ++i) {
-            if (count[i] == N - 1) return i;
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>arr1(n+1);
+        vector<int>arr2(n+1);
+        for(int i = 0;i < trust.size();i++){
+            arr1[trust[i][0]]++;   
+            arr2[trust[i][1]]++;
+        }
+        for(int i = 1;i <= n;i++){
+            if(arr1[i] == 0 && arr2[i] == n-1)return i;
         }
         return -1;
     }
