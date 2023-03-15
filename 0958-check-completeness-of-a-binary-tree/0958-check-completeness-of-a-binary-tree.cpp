db5be25a966023e92ac19410148f9647f0edf9cf
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2023-03-15 09:11:11 +0530

    Time: 5 ms (57.38%), Space: 10.5 MB (29.89%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+
+        bool gotNullSoFar = false;
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+
+            if(node==NULL){
+                gotNullSoFar = true;
+            }else{
+                if(gotNullSoFar==true) return false;
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        return true;
+    }
+};
