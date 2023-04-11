6f91fcac04263b74089deda6259a941bb852be32
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2023-04-11 08:31:44 +0530

    Time: 85 ms (67.67%), Space: 25.7 MB (81.24%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string c="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='*'){
+                c.pop_back();
+            }else{
+                c+=s[i];
+            }
+            
+        }
+        return c;
+    }
+};
