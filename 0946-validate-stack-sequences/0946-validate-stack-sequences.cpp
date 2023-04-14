2d9754362d3eb40b58c11d39dd126dfbba960173
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2023-04-14 09:44:39 +0530

    Time: 16 ms (11.78%), Space: 15 MB (99.61%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,17 +1,16 @@
-class Solution {
+class Solution
+{
 public:
-    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        stack<int> st; // Create a stack
-        
-        int j = 0; // Intialise one pointer pointing on popped array
-        
-        for(auto val : pushed){
-            st.push(val); // insert the values in stack
-            while(st.size() > 0 && st.top() == popped[j]){ // if st.peek() values equal to popped[j];
-                st.pop(); // then pop out
-                j++; // increment j
-            }
+    bool validateStackSequences(vector<int> &push, vector<int> &pop)
+    {
+        int i = 0, j = 0, k = 0, n = push.size();
+        while (k < n)
+        {
+            push[i] = push[k];
+            i++, k++;
+            while (i > 0 && push[i - 1] == pop[j])
+                --i, ++j;
         }
-        return st.size() == 0; // check if stack is empty return true else false
+        return i == 0;
     }
 };
