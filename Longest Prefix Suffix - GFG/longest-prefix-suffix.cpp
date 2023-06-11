95f19e900274c6758c40fec7ca4c51da804676fe
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2023-06-11 11:42:25 +0530

    Added solution - LeetHub

---
+++ Longest Prefix Suffix - GFG/longest-prefix-suffix.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial template for C++ 
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//User function template for C++
+
+class Solution{
+  public:		
+	int lps(string s) {
+	    // Your code goes here
+	    int n = s.size();
+for(int i = 1;i < n;i++){
+    if(s[i] == s[0])
+	if(s.substr(0,n-i).compare(s.substr(i)) == 0) return n-i;
+    }
+return 0;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string str;
+   		cin >> str;
+
+   		Solution ob;
+
+   		cout << ob.lps(str) << "\n";
+   	}
+
+    return 0;
+}
+
+// } Driver Code Ends
