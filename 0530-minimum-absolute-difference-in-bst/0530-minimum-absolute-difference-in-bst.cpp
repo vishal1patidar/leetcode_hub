ca5ec021b205495274a462cf62eb08018d2148c2
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2023-06-14 18:57:24 +0530

    Time: 23 ms (41.79%), Space: 25.5 MB (39.09%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+vector<int>v;
+
+  void inorder(TreeNode* root){
+
+      if(root == NULL)
+       return ;
+
+      inorder(root->left);
+      v.push_back(root->val);
+      inorder(root->right); 
+
+  }
+    int getMinimumDifference(TreeNode* root) {
+        
+         inorder(root);
+
+        int ans=INT_MAX;
+       for(int i=1;i<v.size();i++) 
+        ans= min(ans,v[i]-v[i-1]);
+
+        return ans;
+    }
+};
