fa740a1b42906850ea00c08db8de012d62583ab6
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2023-07-21 09:45:29 +0530

    Time: 337 ms (62.30%), Space: 10.4 MB (84.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,12 +1,14 @@
-class Solution { // 256 ms, faster than 42.84%
+class Solution { 
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, 1);
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < i; ++j)
-                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
-                    dp[i] = dp[j] + 1;
+                // if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                //     dp[i] = dp[j] + 1;
+                    if (nums[i] > nums[j])
+                        dp[i] = max(dp[i], dp[j] + 1);
         return *max_element(dp.begin(), dp.end());
     }
 };
