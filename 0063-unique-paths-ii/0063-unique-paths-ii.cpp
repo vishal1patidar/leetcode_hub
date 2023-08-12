e949c723b602c7b47b4970a145128f6b170c044e
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2023-08-12 18:30:50 +0530

    Time: 5 ms (28.03%), Space: 7.8 MB (25.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,72 +1,28 @@
-// Here is my c++ code for this problem.
-
-// Memoization
-// Complexity
-// Time complexity:O(n*m)
-
-// Space complexity:O(n*m)+O(n)
-
-// Code
-// class Solution {
-// public:
-//     int f(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
-//         if(i==0 &&j==0 && obstacleGrid[i][j]!=1){return 1;}
-//         if(i<0 || j<0){return 0;}
-//         if(obstacleGrid[i][j]==1){return 0;}
-//         if(dp[i][j]!=-1){return dp[i][j];}
-//         return dp[i][j]=f(i-1, j, obstacleGrid, dp)+f(i, j-1, obstacleGrid, dp);
-//     }
-//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-//         int n=obstacleGrid.size(), m=obstacleGrid[0].size();
-//         vector<vector<int>>dp(n, vector<int>(m, -1));
-//         return f(n-1, m-1, obstacleGrid, dp);
-//     }
-// };
-// Tabulation
-// Complexity
-// Time complexity:O(n*m)
-
-// Space complexity:O(n*m)
-
-// Code
-// class Solution {
-// public:
-//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-//         int n=obstacleGrid.size(), m=obstacleGrid[0].size();
-//         vector<vector<int>>dp(n, vector<int>(m, 0));
-//         for(int i=0; i<n; i++){
-//             for(int j=0; j<m; j++){
-//                 if(obstacleGrid[i][j]==1){continue;}
-//                 if(i==0 && j==0 && obstacleGrid[i][j]!=1){dp[i][j]=1;}
-//                 if(i>0){dp[i][j]+=dp[i-1][j];}
-//                 if(j>0){dp[i][j]+=dp[i][j-1];}
-//             }
-//         }
-//         return dp[n-1][m-1];
-//     }
-// };
-// Tabulation - Space Optimization
-// Complexity
-// Time complexity:O(n*m)
-
-// Space complexity:O(n)
-
-//Code
 class Solution {
 public:
+    
+    int solve(int i, int j, vector<vector<int>> &dp, vector<vector<int>>& grid)
+    {
+        if(grid[i][j]==1) return 0;
+        
+        if(i==0 and j==0) return 1;
+        if(i==0) return dp[i][j-1];
+        if(j==0) return dp[i-1][j]; 
+            
+        return dp[i-1][j] + dp[i][j-1];
+    }
+    
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n=obstacleGrid.size(), m=obstacleGrid[0].size();
-        vector<int>dp(m, 0);
-        for(int i=0; i<n; i++){
-            vector<int>temp(m, 0);
-            for(int j=0; j<m; j++){
-                if(obstacleGrid[i][j]==1){continue;}
-                if(i==0 && j==0 && obstacleGrid[i][j]!=1){temp[j]=1;}
-                if(i>0){temp[j]+=dp[j];}
-                if(j>0){temp[j]+=temp[j-1];}
-            }
-            dp=temp;
-        }
-        return dp[m-1];
+        int n = obstacleGrid.size(), m = obstacleGrid[0].size();
+        
+        if(obstacleGrid[n-1][m-1]==1) return 0;
+        
+        vector<vector<int>> dp(n, vector<int> (m,0));
+        
+        for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+        dp[i][j] = solve(i,j,dp, obstacleGrid);
+        
+        return dp[n-1][m-1];
     }
 };
